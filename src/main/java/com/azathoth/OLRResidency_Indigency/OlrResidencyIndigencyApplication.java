bb38c7abcb71f85f811529f376b2c8cf745cb7d6
BenjiBenji20 <imperialbenjie393@gmai.com>
2025-03-01 16:04:05 +0800

    First commit. Starting the project using Spring framework. Starting to configure security using OAuth2 framework.

---
+++ src/main/java/com/azathoth/OLRResidency_Indigency/OlrResidencyIndigencyApplication.java
@@ -0,0 +1,13 @@
+package com.azathoth.OLRResidency_Indigency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlrResidencyIndigencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlrResidencyIndigencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azathoth/OLRResidency_Indigency/OlrResidencyIndigencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azathoth.OLRResidency_Indigency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlrResidencyIndigencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
